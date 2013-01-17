c375e6536ed4e3843da356603ef9957e4c2fb5e9
Steve Lee <sphen.lee@gmail.com>
2013-01-17 22:08:37 +1100

    map rendering - progress

--- engine/map.py
+++ engine/map.py
@@ -8,24 +8,42 @@ import pyglet
 from pyglet import gl
 
 import array
+import struct
 
 class Sector(object):
     def __init__(self):
-        self.terrain = pyglet.image.load('maps/test/grass.png')
+        self.terrain = pyglet.image.load('maps/test/grass.png').get_texture()
+
+    class Group(pyglet.graphics.Group):
+        def __init__(self, tex):
+            super(Sector.Group, self).__init__()
+            self.tex = tex
+
+        def set_state(self):
+            gl.glEnable(self.tex.target)
+            gl.glBindTexture(self.tex.target, self.tex.id)
+
+        def unset_state(self):
+            gl.glDisable(self.tex.target)
 
     def rendersetup(self, batch):
-        self.vb = batch.add(6, gl.GL_TRIANGLES, None, 'v2f', 'c3B')
+        with open('maps/test/node.obj.raw') as fp:
+            data = fp.read()
 
-        a = array.array('f', [
-            0.0, 0.0,
-            100.0, 0.0,
-            100.0, 100.0,
-            100.0, 100.0,
-            0.0, 0.0,
-            0.0, 100.0,
-        ])
-        self.vb.vertices = a
-        self.vb.colors = [255, 0, 0] * 6
+        s = struct.Struct('I')
+        n, = s.unpack(data[0:4])
+
+        vdata = array.array('f')
+        vdata.fromstring(data[4:4+(n*4)])
+
+        tdata = array.array('f')
+        tdata.fromstring(data[4+(n*4):])
+
+        group = self.Group(self.terrain)
+        self.vb = batch.add(n/2, gl.GL_TRIANGLES, group, 'v2f', 't2f')
+
+        self.vb.vertices = vdata
+        self.vb.tex_coords = tdata
 
 class Map(object):
     def __init__(self):
